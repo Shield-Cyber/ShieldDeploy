71d1840797fea7dd5c2b4c8a99bd77af3eef1322
Oliver Scotten <revi99@me.com>
2023-06-12 13:24:06 -0400

    Progress

--- AgentInstall.ps1
+++ AgentInstall.ps1
@@ -1,29 +1,45 @@
 $installDir = "C:\ShieldAgent"
 
-# @REM Make Dir
-New-Item -ItemType Directory -Path $installDir
+$resetScratch = $null
 
+# Make Dir If Dir Exists, ask to reinstall.
+if (Test-Path $installDir -PathType Container) {
+    Write-Output "Install Directory exists. Would you like to reinstall?"
+    while ($resetScratch -ne "Y" -and $resetScratch -ne "N") {
+        $resetScratch = Read-Host "Reinstall? (Y/N)"
+        $resetScratch = $resetScratch.ToUpper()
+    }
+} else {
+    Write-Output "Install Directory does not exist."
+    New-Item -ItemType Directory -Path $installDir
+}
+if ($resetScratch -eq "Y") {
+    Write-Host "You chose YES."
+    Remove-Item -Path $installDir -Recurse -Force
+    New-Item -ItemType Directory -Path $installDir
+}
+else {
+    Write-Host "You chose NO."
+}
 
-# Check if file already downloaded.
-# @REM Download ZIP File
+# Download ZIP File
 $url = "https://shield-agent.s3.amazonaws.com/ShieldAgent.zip"
 $zipFilePath = $installDir + "\ShieldAgent.zip"
 
-# $url = "http://example.com/path/to/file.zip"
-# $outputPath = "C:\Path\to\Save\file.zip"
+if (Test-Path $zipFilePath -PathType Leaf) {
+    Write-Output "Zip file exists. Delete this directory if you wish to reinstall. $installDir"
+} else {
+    Write-Output "Zip file does not exist. Downloading / Unzipping."
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($url, $zipFilePath)
 
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($url, $zipFilePath)
+    $webClient.Dispose()
 
-$webClient.Dispose()
+    # Unzip File
+    Expand-Archive -Path $zipFilePath -DestinationPath (Split-Path -Path $zipFilePath -Parent)
+}
 
-# Invoke-WebRequest -Uri $url -OutFile $zipFilePath
-
-# Check if File already unzipped.
-# @REM Unzip File
-Expand-Archive -Path $zipFilePath -DestinationPath (Split-Path -Path $zipFilePath -Parent)
-
-# @REM Prompt Y/N for Nessus
+# Prompt Y/N for Nessus
 $confirmation = $null
 
 while ($confirmation -ne "Y" -and $confirmation -ne "N") {
@@ -33,35 +49,42 @@ while ($confirmation -ne "Y" -and $confirmation -ne "N") {
 
 if ($confirmation -eq "Y") {
     Write-Host "You chose YES."
-    # @REM Update AppSettings.json
+    # Update AppSettings.json
 }
 else {
     Write-Host "You chose NO."
-    # @REM Continue
 }
 
-# Check creds before doing thing and ask again if wrong.
-# @REM Prompt Username/Password/Domain
+# Prompt Username/Password/Domain
 $credential = Get-Credential
 $agentUsername = $credential.UserName
 $agentPassword = $credential.GetNetworkCredential().Password
-$agentDomain = $credential.GetNetworkCredential().Domain
-$domainUsername = $agentDomain + "\" + $agentUsername
 
-# @REM Install Agent.exe as Service
-$serviceName = "ShieldAgent2"
+Write-Host $agentUsername
+
+# Install Agent.exe as Service
+$serviceName = "ShieldAgent"
 $binPath = $installDir + "\HopliteAgent\HopliteShield.Agent.exe"
 $startType = "delayed-auto"
 
-Write-Output "sc.exe create $serviceName binpath=$binPath start=$startType obj=.\$agentUsername password=$agentPassword"
+$completed = $false
 
-# @REM sc.exe Command
-sc.exe create $serviceName binpath=$binPath start=$startType obj=".\$agentUsername" password=$agentPassword
+while (-not $completed) {
+    try {        
+        sc.exe delete $serviceName
 
-# Start-Process -NoNewWindow -Wait -FilePath "sc.exe" -ArgumentList $arguments
+        sc.exe create $serviceName binpath= $binPath start= $startType obj= $agentUsername password= $agentPassword
 
-# @REM Start Service
-# Start-Service -Name $serviceName
+        $completed = $true
+    }
+    catch {
+        Write-Output "Failed to authenticate. Please check your credentials."
 
-# @REM Confirm Service is Running
-# @REM Prompt Reinstall Y/N
+        $credential = Get-Credential
+        $agentUsername = $credential.UserName
+        $agentPassword = $credential.GetNetworkCredential().Password
+    }
+}
+
+# Start Service
+Start-Service -Name $serviceName
